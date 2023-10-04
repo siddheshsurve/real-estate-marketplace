968b6d10205f727dd8e0c12a289cb7a401660793
siddheshsurve <sysurve24@gmail.com>
2023-10-04 18:02:47 +0530

    Link to routes added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import {FaSearch} from "react-icons/fa"
+import {Link} from "react-router-dom"
 
 
 export default function Header() {
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-            <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-                <span className='text-slate-500'>Siddhesh</span>
-                <span className='text-slate-700'>Estate</span>
-            </h1>
+            <Link to='/'>
+                <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                    <span className='text-slate-500'>Siddhesh</span>
+                    <span className='text-slate-700'>Estate</span>
+                </h1>
+            </Link>
 
             <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
                 <input 
@@ -21,9 +24,17 @@ export default function Header() {
             </form>
 
             <ul className='flex gap-4'>
-                <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
-                <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
-                <li className='hidden sm:inline text-slate-700 hover:underline'>SignIn</li>
+                <Link to='/'>
+                    <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+                </Link>
+
+                <Link to='/about'>
+                    <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+                </Link>
+
+                <Link to='/sign-in'>
+                    <li className='text-slate-700 hover:underline'>SignIn</li>
+                </Link>
             </ul>
         </div>
         
